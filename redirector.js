f2a2aa6c088d0ef3125c4414c044e94b1a276e88
emailcuathuc <64247194+emailcuathuc@users.noreply.github.com>
2021-10-02 01:44:41 +0700

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -1,2 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
 /* Redirector */
-(function(b){window.top.location.href.includes("/chuyen-trang.html")||b.filter(function(a){return!a.hasAttribute("imageanchor")&&!a.href.startsWith("https://danhgia.sunghiephoc.com")&&5<a.href.length}).map(function(a){a.href="https://danhgia.sunghiephoc.com/p/chuyen-trang.html#"+encodeURIComponent(a.href);a.target="_blank";a.rel="nofollow noopener"})})(Array.from(document.querySelectorAll(".post-body a")));
+(function(b){window.top.location.href.includes("/chuyen-huong-khoi-blog.html")||b.filter(function(a){return!a.hasAttribute("imageanchor")&&!a.href.startsWith("https://danhgia.sunghiephoc.com/")&&5<a.href.length}).map(function(a){a.href="/p/chuyen-huong-khoi-blog.html?ntlruby="+(a.href);a.rel="nofollow noopener"})})(Array.from(document.querySelectorAll(".post-body a")));
+//]]></script>
